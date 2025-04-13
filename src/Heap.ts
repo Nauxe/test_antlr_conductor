398db25491463543d857fc8722b3535e2a309458
Nauxe <51425614+Nauxe@users.noreply.github.com>
2025-04-14 00:15:27 +0800

    Add allocEnv

--- src/Heap.ts
+++ src/Heap.ts
@@ -4,6 +4,7 @@ export enum Tag {
   NUMBER = 2, // Primitive
   BOOLEAN = 3, // Primitive
   TUPLE = 4, // Heap allocated
+  ENVIRONMENT = 5, // Heap allocated
 }
 
 const WORD_SIZE = 8;
@@ -19,11 +20,13 @@ export class Heap {
     this.dataView = new DataView(this.data);
 
     this.free = 0; // -1 if no more space
-    this.set_child(this.free, -1);
+    this.set_tag(this.free, Tag.FREE);
+    this.set_child(this.free, 0, -1);
 
     // Initialize free list
     for (let i: number = WORD_SIZE; i < heapSize; i += WORD_SIZE) {
-      this.set_child(i, this.free);
+      this.set_tag(i, Tag.FREE);
+      this.set_child(i, 0, this.free);
       this.free = i;
     }
   }
@@ -31,7 +34,7 @@ export class Heap {
   // Word size is 8
   // [1 byte tag, 4 bytes data, 2 bytes children, 1 byte unused]
   // If 4 bytes is not enough for the size required, more words
-  // are used on the heap and the first child will point to the 
+  // are used on the heap and the second child will point to the 
   // heap memory address of the next part of the data
   allocate(tag: Tag, size: number): Operand {
     if (this.free == -1 && this.get_tag(this.free) == Tag.FREE) {
@@ -40,13 +43,13 @@ export class Heap {
 
     const op = new Operand(tag, this.free);
     for (let i = 4; i < size; i += 4) {
-      this.free = this.get_child(this.free);
+      this.free = this.get_child(this.free, 0);
       if (this.free == -1 && this.get_tag(this.free) == Tag.FREE) {
         throw new Error("No more space on heap!");
       }
       this.set_tag(this.free, Tag.PARTIAL);
     }
-    this.free = this.get_child(this.free);
+    this.free = this.get_child(this.free, 0);
     return op;
   }
 
@@ -59,14 +62,20 @@ export class Heap {
   }
 
   // addr is the address of the start of a word
-  set_child(addr: number, value: number) {
-    this.data[addr + 5] = value;
+  set_child(addr: number, childID: number, value: number) {
+    this.data[addr + 5 + childID] = value;
   }
 
   // addr is the address of the start of a word
-  get_child(addr: number): number {
-    return this.data[addr + 5]
+  get_child(addr: number, childID: number): number {
+    return this.data[addr + 5 + childID];
   }
+
+  // Allocate an environment
+  allocEnv(frameSize: number): Operand {
+    return this.allocate(Tag.ENVIRONMENT, frameSize);
+  }
+
 }
 
 // Type containing either a primitive or an address depending on the tag
