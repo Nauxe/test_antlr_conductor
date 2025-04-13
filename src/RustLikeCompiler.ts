ae33e7c9c2ddc8c2adc99a8cce74f5040f9279dd
Nauxe <51425614+Nauxe@users.noreply.github.com>
2025-04-14 01:44:30 +0800

    Add compiler skeleton

---
+++ src/RustLikeCompiler.ts
@@ -0,0 +1,25 @@
+import { Tag, Operand, Inst } from "./RustLikeVirtualMachine";
+
+export class RustCompiler {
+  private WC: number; // Write counter
+  private instrs: Inst[];
+
+  constructor() {
+    this.WC = 0;
+    this.instrs = [];
+  }
+
+  // TODO:
+  scan_for_locals() {
+
+  }
+
+  // TODO:
+  compile_sequence() {
+
+  }
+
+  //TODO: 
+  compile_comp(comp) {
+  }
+}
