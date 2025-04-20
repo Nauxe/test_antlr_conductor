1c81d890d656f0526ac0f9e41485eeca373ca98a
Nauxe <51425614+Nauxe@users.noreply.github.com>
2025-04-20 18:15:21 +0800

    Move parser visitor into compiler file

--- src/RustLikeCompiler.ts
+++ src/RustLikeCompiler.ts
@@ -1,5 +1,57 @@
 //import { Tag, HeapItem } from "./Heap";
-import { Inst } from "./RustLikeVirtualMachine";
+import { Bytecode, Inst } from "./RustLikeVirtualMachine";
+import { CharStream, CommonTokenStream, AbstractParseTreeVisitor } from 'antlr4ng';
+import { ExpressionContext, ProgContext, RustLikeParser } from './parser/grammar/RustLikeParser';
+import { RustLikeVisitor } from './parser/grammar/RustLikeVisitor';
+
+export class RustLikeCompilerVisitor extends AbstractParseTreeVisitor<number> implements RustLikeVisitor<number> {
+  // Visit a parse tree produced by RustLikeParser#prog
+  visitProg(ctx: ProgContext): number {
+    return this.visit(ctx.block());
+  }
+
+  // Visit a parse tree produced by RustLikeParser#expression
+  visitExpression(ctx: ExpressionContext): number {
+    if (ctx.getChildCount() === 1) {
+      // INT case
+      return parseInt(ctx.getText());
+    } else if (ctx.getChildCount() === 3) {
+      if (ctx.getChild(0).getText() === '(' && ctx.getChild(2).getText() === ')') {
+        // Parenthesized expression
+        return this.visit(ctx.getChild(1) as ExpressionContext);
+      } else {
+        // Binary operation
+        const left = this.visit(ctx.getChild(0) as ExpressionContext);
+        const op = ctx.getChild(1).getText();
+        const right = this.visit(ctx.getChild(2) as ExpressionContext);
+
+        switch (op) {
+          case '+': return left + right;
+          case '-': return left - right;
+          case '*': return left * right;
+          case '/':
+            if (right === 0) {
+              throw new Error("Division by zero");
+            }
+            return left / right;
+          default:
+            throw new Error(`Unknown operator: ${op}`);
+        }
+      }
+    }
+
+    throw new Error(`Invalid expression: ${ctx.getText()}`);
+  }
+
+  // Override the default result method from AbstractParseTreeVisitor
+  protected defaultResult(): number {
+    return 0;
+  }
+  // Override the aggregate result method
+  protected aggregateResult(aggregate: number, nextResult: number): number {
+    return nextResult;
+  }
+}
 
 export class RustCompiler {
   private WC: number; // Write counter
